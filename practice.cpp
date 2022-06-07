e2f0a3f69188df69ec4065de5abe3acc8f39073a
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-06-07 13:10:08 +0600

    C++ STL basic practice added

--- practice.cpp
+++ practice.cpp
@@ -1,9 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main(){
 
-	cout << "Hello World!";
+	int ar[6] =  {1, 2, 3, 4, 5, 6};
+	int arr[6];
+
+	copy_n(ar, 6, arr);
+
+	vector<int> vec1 = {1, 2, 3, 4, 5, 6};
+	vector<int> vec2 = {6, 5, 4, 3, 2, 1};
+
+	swap(vec1, vec2);
+	// vec1.clear();
+
+	for(auto i: vec1){
+		cout << i << endl;
+	}
+
+	// Vector pair
+	vector <pair <string, int>> pvec;
+	pvec.push_back(make_pair("Ferdous", 23));
+	cout << pvec[0].first;
+
+
 
 	return 0;
 }
