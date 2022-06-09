fa162efcbbbd6bbac654e152d2ccb9af227950ae
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-06-09 15:01:10 +0600

    pairs O(nlogn) added

--- pairs.cpp
+++ pairs.cpp
@@ -6,13 +6,18 @@ int main(){
 	vector<int> nums = {10, 5, 2 , 3, -6, 9 , 11 };
 	int target = 4;
 
+	sort(nums.begin(), nums.end());
+
 	for(int i = 0; i < nums.size() - 1; i++){
+
+		int rem = target - nums[i];
+
 		for(int j = i + 1; j < nums.size(); j++){
-			if(nums[i] + nums[j] == target){
+			if(nums[j] == rem){
 				cout << nums[i] << " : " << nums[j] << endl;
 			}
 		} 
 	}
 }
 
-// Time complexity: O(n^2)
+// Time complexity: O(n log n)
